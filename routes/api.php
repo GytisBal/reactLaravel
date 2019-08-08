81b2016278ae1906110f72b2ada89aeea7853140
Gytis <gytisbalciunas1551@gmail.com>
2019-08-08 17:09:02 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('admin', function () {
+    return view('admin');
+});
+
 Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
